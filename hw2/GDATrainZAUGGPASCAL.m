ef1ece519d534fa061dd81f4cde0f759691b5c34
Pascal Zaugg <pascal.zaugg@students.unibe.ch>
2013-10-27 11:15:12 +0100

    Pushed transposing mu1 and mu0 down
    mu1 and mu0 are now transposed at the very end of the
    GDATrain function so I don't have to transpose it
    when using bsxfun.

--- hw2/GDATrainZAUGGPASCAL.m
+++ hw2/GDATrainZAUGGPASCAL.m
@@ -3,8 +3,9 @@ function [phi, mu0, mu1, Sigma] = GDATrainZAUGGPASCAL( DataTrain, LabelsTrain )
   %convert labels from -1 to 0
   LabelsTrain(LabelsTrain == -1) = 0;
 
-  %getting dimension and m
+  %getting dimension (number of features)
   dim = size(DataTrain,2);
+  %getting number of samples
   m = size(LabelsTrain, 2);
 
   %create index vector for 1
@@ -35,8 +36,9 @@ function [phi, mu0, mu1, Sigma] = GDATrainZAUGGPASCAL( DataTrain, LabelsTrain )
 
   %sum all rows which correspond to the label 0
   result = sum(negative, 1);
-  %divide result by the count of labels
-  mu0 = transpose(result / sumOfZeros);
+  %divide result by the count of labels. mu0 is at the
+  %moment a row vector
+  mu0 = result / sumOfZeros;
 
   %
   % --- calculate mu1 ----
@@ -44,20 +46,27 @@ function [phi, mu0, mu1, Sigma] = GDATrainZAUGGPASCAL( DataTrain, LabelsTrain )
 
   %sum all rows which correspond to the label 1
   result = sum(positive, 1);
-  %divide result by the count of labels
-  mu1 = transpose(result / sumOfOnes);
+  %divide result by the count of labels. mu1 is
+  %at the moment a row vector
+  mu1 = result / sumOfOnes;
   
   %
   % --- calculate sigma ---
   %
 
   %subtract mu0 from each row
-  negative = bsxfun(@minus, negative, mu0');
+  negative = bsxfun(@minus, negative, mu0);
 
   %subtract mu1 from each row
-  positive = bsxfun(@minus, positive, mu1');
+  positive = bsxfun(@minus, positive, mu1);
 
   %part of sigma is sum x(i)-mu * x(i)'-mu which is exaclty the same as A'*A when
   %mu is already subtracted. 
   Sigma = ((positive' * positive)+(negative' * negative))/m;
+
+  %change mu1 and mu0 to column vector
+  %I pushed this down here so I don't have to
+  %transpose it when using bsxfun.
+  mu0 = mu0';
+  mu1 = mu1';
 end
