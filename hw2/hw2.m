cd9fea5787407e8af6be4932d8d30f163465f2a3
Pascal Zaugg <pascal.zaugg@students.unibe.ch>
2013-10-27 10:44:02 +0100

    Added tic; toc; again
    Which was commented out for testing reasons.

--- hw2/hw2.m
+++ hw2/hw2.m
@@ -68,10 +68,10 @@ LabelsTest = Labels(1,testIdx);
 %%
 % Training with Gaussian Discriminant Analysis
 fprintf('Training ... ');
-%tic;
+tic;
 [phi, mu0, mu1, Sigma] = GDATrainZAUGGPASCAL( DataTrain, LabelsTrain );
 % [phi, mu0, mu1, Sigma] = GDATrain_SOLUTION( DataTrain, LabelsTrain );
-%toc;
+toc;
 
 %%
 % Test Gaussian Discriminant Analysis.
