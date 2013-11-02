8902c7eef83850ce9b9c47235e9d029bf9a2c9a6
Pascal Zaugg <pascal.zaugg@students.unibe.ch>
2013-11-02 10:30:24 +0100

    First simple example to work with vlfeat
    Created a very simple 2D example with 4 samples in
    testSVM.m and plotted them. Working really fine. :)

---
+++ hw3/testSVM.m
@@ -0,0 +1,40 @@
+close all;
+clear all;
+clc;
+
+%%
+% Training 
+fprintf('Training ... ');
+VLFEAT_FOLDER = '../vlfeat'; %Put here the path to the root floder of VlFeat
+
+addpath ([VLFEAT_FOLDER, '/toolbox']);
+vl_setup;
+
+X = [1, 2; 2, 1; 5, 7; 7, 5];
+y = [1; 1; -1; -1;];
+
+X = X';
+y = y';
+
+Xp = X(:, y==1);
+Xn = X(:, y==-1);
+
+f = figure;
+set(f, "visible", "off");
+
+plot(Xn(1,:),Xn(2,:),'*r')
+hold on
+plot(Xp(1,:),Xp(2,:),'*b')
+axis equal ;
+
+lambda = 0.01 ; % Regularization parameter
+maxIter = 1000 ; % Maximum number of iterations
+
+[w b info] = vl_svmtrain(X, y, lambda, 'MaxNumIterations', maxIter);
+
+% Visualisation
+eq = [num2str(w(1)) '*x+' num2str(w(2)) '*y+' num2str(b)];
+line = ezplot(eq);
+set(line, 'Color', [0 0.8 0],'linewidth', 2);
+
+print(f, "testResultPlot.png", "-dpng");
